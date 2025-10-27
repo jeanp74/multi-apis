e52342c5429f8a1daddc3e48c351a162241ef4f0
jeanp74 <jeanpaulsolano74@gmail.com>
2025-10-27 12:30:03 -0500

    4

--- users-api/src/app.js
+++ users-api/src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import { pool } from "./db.js";
 import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
